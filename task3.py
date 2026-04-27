6c2a7aa2347a1b6a1b8550b9fd3ce38abbdaf670
madridabdiroziqov-ops <madridabdiroziqov@gmail.com>
2026-04-27 19:06:30 +0500

    Delete task3.py

--- task3.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-def rearrange_by_frequency(nums: list[int]) -> list[int]:
-    freq = Counter(nums)
-    return sorted(nums, key=lambda x: (-freq[x], x))
-
-
-print(rearrange_by_frequency([4, 5, 6, 5, 4, 3, 4]))
